45c41306cfd57b2dc0cbf0a3d4e136ab975aa30b
Aaron <1092099510@qq.com>
2021-04-14 20:23:52 +0800

    训练界面添加日志显示

--- src/api/user.js
+++ src/api/user.js
@@ -5,7 +5,7 @@ export function login(params) {
     url: '/user/login',
     method: 'post',
     params: params,
-    baseURL: 'http://192.168.19.239:8083'
+    baseURL: 'http://192.168.19.239:8088'
     //data,
     //baseURL: 'http://localhost:9528/dev-api'
   })
@@ -17,7 +17,7 @@ export function getInfo(token) {
     url: '/user/info',
     method: 'get',
     params: { token },
-    baseURL: 'http://192.168.19.239:8083'
+    baseURL: 'http://192.168.19.239:8088'
     //baseURL: 'http://localhost:9528/dev-api'
   })
 }
@@ -27,7 +27,7 @@ export function logout() {
     url: '/user/logout',
     method: 'post',
     //params: { token },
-    baseURL: 'http://192.168.19.239:8083'
+    baseURL: 'http://192.168.19.239:8088'
     //baseURL: 'http://localhost:9528/dev-api'
   })
 }
