76e4ac11cb780b2644a4f047b9718fb6134a9024
liaoziheng <15529377256@163.com>
2021-10-25 18:53:03 +0800

    1025整合

--- src/api/user.js
+++ src/api/user.js
@@ -5,7 +5,8 @@ export function login(params) {
     url: '/user/login',
     method: 'post',
     // params: params,
-    baseURL: 'http://localhost:8088',
+    baseURL: 'http://10.19.1.77:8088',
+    //baseURL: 'http://localhost:8088',
     data: params
     //baseURL: 'http://localhost:9528/dev-api'
    })
@@ -17,7 +18,8 @@ export function getInfo(token) {
     url: '/user/info',
     method: 'get',
     params: { token },
-    baseURL: 'http://localhost:8088'
+    baseURL: 'http://10.19.1.77:8088',
+    //baseURL: 'http://localhost:8088',
     //baseURL: 'http://localhost:9528/dev-api'
   })
 }
@@ -27,7 +29,8 @@ export function logout() {
     url: '/user/logout',
     method: 'post',
     //params: { token },
-    baseURL: 'http://localhost:8088'
+    baseURL: 'http://10.19.1.77:8088',
+    //baseURL: 'http://localhost:8088',
     //baseURL: 'http://localhost:9528/dev-api'
   })
 }
